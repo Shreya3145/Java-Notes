8717d2d2e9a87756c3bbf52ddf788330dcdc085e
Shreya Ravichandiran <145599967+Shreya3145@users.noreply.github.com>
2025-01-22 03:36:26 +0530

    Update Ques2.java

--- Test_Preparation/Ques2.java
+++ Test_Preparation/Ques2.java
@@ -1,5 +1,18 @@
 package Test_Preparation;
+import java.util.Scanner;
 
 public class Ques2 {
-    
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+        boolean isSummer = sc.nextBoolean();
+        if((temp>60 && temp<90 && isSummer == false)||(isSummer && temp >= 60 && temp <= 100)){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+        
+    }
 }
